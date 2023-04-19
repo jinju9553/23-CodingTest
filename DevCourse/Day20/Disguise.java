01555286436956bf67f16285fb97848fca7aa916
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2023-04-19 15:34:10 +0900

    230419 문제풀이

---
+++ DevCourse/Day20/Disguise.java
@@ -0,0 +1,24 @@
+package DevCourse.Day20;
+
+import java.util.*;
+
+public class Disguise {
+
+	private static Map<String, Integer> map = new HashMap<>();
+    
+    public int solution(String[][] clothes) {
+        int answer = 1;
+                
+        for(String[] c : clothes) {
+            String type = c[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        
+        for(int v : map.values()) {
+            answer *= (v + 1);
+        }
+    
+        return (answer - 1);
+    }
+
+}
