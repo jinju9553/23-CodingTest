a78183ec7565c1342749f1134045dd19c6f45a0f
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2023-04-21 21:49:12 +0900

    230421 문제풀이 추가

---
+++ DevCourse/Day22/Sticker.java
@@ -0,0 +1,40 @@
+package DevCourse.Day22;
+
+public class Sticker {
+
+	public int solution(int sticker[]) {
+        int answer = 0;
+
+        int size = sticker.length;
+        int[] DP = new int[size];
+        
+        if(size <= 1) {
+            return sticker[0];
+        }
+        
+        //첫 번째 스티커를 사용하는 경우
+        DP[0] = sticker[0];
+        DP[1] = DP[0]; //주의: 자동으로 두 번째 스티커는 버림
+
+        for(int i = 2; i < size - 1; i++) {
+            DP[i] = Math.max(DP[i - 1], DP[i - 2] + sticker[i]);
+        }
+        
+        int result1 = DP[size - 2];
+        
+        //마지막 스티커를 사용하는 경우
+        DP[0] = 0;
+        DP[1] = sticker[1];
+        
+        for(int i = 2; i < size; i++) {
+            DP[i] = Math.max(DP[i - 1], DP[i - 2] + sticker[i]);
+        }
+        
+        int result2 = DP[size - 1];
+
+        answer = Math.max(result1, result2);
+        
+        return answer;
+    }
+
+}
