85da490a1380beeb8e1e4643394cf646ad122889
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2023-04-27 18:43:14 +0900

    230427 문제풀이

---
+++ DevCourse/Day26/NumberGame.java
@@ -0,0 +1,53 @@
+package DevCourse.Day26;
+
+import java.util.Arrays;
+
+public class NumberGame {
+
+	public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int key : A) {
+            int result = binarySearch(B, key);
+            if(result == -1){
+                continue;
+            }
+            
+            if(B[result] > key) {
+                answer += 1;
+            }
+            
+            B[result] = -1;
+        }
+        
+        return answer;
+    }
+    
+    public static int binarySearch(int[] B, int key) {
+        int start = 0;
+        int end = B.length - 1;
+        
+        int result = Integer.MAX_VALUE; //ÀÎµ¦½º ÀúÀå
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            
+            if(B[mid] > key) {
+                result = Math.min(result, mid);
+                end = mid - 1;
+            }
+            else { //B[mid] <= key
+                start = mid + 1;
+            }
+        }
+        
+        if(result == Integer.MAX_VALUE) {
+            return -1;
+        }
+        
+        return result;
+    }
+
+}
