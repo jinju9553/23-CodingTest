98923033239f706bdab3c14209f444007319a616
Pearlii <69393506+jinju9553@users.noreply.github.com>
2023-08-01 14:15:27 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 24560 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer s = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(s.nextToken());
+		int M = Integer.parseInt(s.nextToken());
+		
+		int[] items = new int[N];
+		int[] bucket = new int[M];
+		
+		for(int i = 0; i < N; i++)
+			items[i] = (i + 1);
+		
+		solution(items, bucket, M);
+		System.out.println(sb);
+	}
+
+	public static void solution(int[] items, int[] bucket, int k) { 
+		
+		if (k == 0) { 
+			for (int i = 0; i < bucket.length; i++)
+				sb.append(bucket[i]).append(' ');
+			sb.append('\n');
+			return;
+		}
+		
+		int lastIndex = bucket.length - k - 1; 
+		
+		for (int i = 0; i < items.length; i++)
+		{
+			int flag = 0;
+			for (int j = 0; j <= lastIndex; j++)
+				if (bucket[j] == items[i]) 
+					flag = 1; 
+			
+			if (flag == 1) 
+				continue;
+			
+			bucket[lastIndex + 1] = items[i]; 
+			solution(items, bucket, k - 1);
+		}
+	}
+}
