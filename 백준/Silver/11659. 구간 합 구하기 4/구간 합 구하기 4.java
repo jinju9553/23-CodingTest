22b2c938a71a5eb19545c1148f76000bb6477123
Pearlii <69393506+jinju9553@users.noreply.github.com>
2023-08-02 13:59:30 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 748 ms, Memory: 58764 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] temp = br.readLine().split(" ");
+
+		int N = Integer.parseInt(temp[0]);
+		int M = Integer.parseInt(temp[1]);
+		
+		int[] numbers = new int[N + 1];
+		int[] sum = new int [N + 1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 1; i <= N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+			sum[i] = sum[i - 1] + numbers[i];
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		while(M-- > 0) {
+			temp = br.readLine().split(" ");
+			
+			int start = Integer.parseInt(temp[0]);
+			int end = Integer.parseInt(temp[1]);
+			
+			int sumOfSections = sum[end] - sum[start - 1];
+			
+			sb.append(sumOfSections).append('\n');
+		}
+		
+		System.out.print(sb);
+	}
+}
